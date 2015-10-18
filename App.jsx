ea260da43b726df845fc665e96348afd7cf49fa6
Aaron Dancer <me@aarondancer.com>
2015-10-17 23:27:19 -0500

    Added user registration and login

--- App.jsx
+++ App.jsx
@@ -1,79 +1,89 @@
 // App component - represents the whole app
 App = React.createClass({
 
-  mixins: [ReactMeteorData],
+	mixins: [ReactMeteorData],
 
-  getInitialState() {
-    return {
-      hideCompleted: false
-    }
-  },
+	getInitialState() {
+		return {
+			hideCompleted: false
+		}
+	},
 
-  // Loads items from the Tasks collection and puts them on this.data.tasks
-  getMeteorData() {
-    let query = {};
+	// Loads items from the Tasks collection and puts them on this.data.tasks
+	getMeteorData() {
+		let query = {};
 
-    if (this.state.hideCompleted) {
-      query = {checked: {$ne: true}};
-    }
+		if (this.state.hideCompleted) {
+			query = {checked: {$ne: true}};
+		}
 
-    return {
-      tasks: Tasks.find(query, {soft: {createdAt: -1}}).fetch(),
-      incompleteCount: Tasks.find({checked: {$ne: true}}).count()
-    };
-  },
+		return {
+			tasks: Tasks.find(query, {soft: {createdAt: -1}}).fetch(),
+			incompleteCount: Tasks.find({checked: {$ne: true}}).count(),
+			currentUser: Meteor.user()
+		};
+	},
 
-  renderTasks() {
-    return this.data.tasks.map((task) => {
-      return <Task key={task._id} task={task} />;
-    });
-  },
+	renderTasks() {
+		return this.data.tasks.map((task) => {
+			return <Task key={task._id} task={task} />;
+		});
+	},
 
-  handleSubmit(event) {
-    event.preventDefault();
+	handleSubmit(event) {
+		event.preventDefault();
 
-    // Find the text field via the React ref
-    var textField = React.findDOMNode(this.refs.textInput);
+		// Find the text field via the React ref
+		var textField = React.findDOMNode(this.refs.textInput);
 
-    Tasks.insert({
-      text: textField.value.trim(),
-      createdAt: new Date()
-    });
+		Tasks.insert({
+			text: textField.value.trim(),
+			createdAt: new Date(),            // current time
+			owner: Meteor.userId(),           // _id of logged in user
+			username: Meteor.user().username  // username of logged in user
+		});
 
-    // Clear form
-    textField.value = "";
-  },
+		// Clear form
+		textField.value = "";
+	},
 
-  toggleHideCompleted() {
-    this.setState({
-      hideCompleted: ! this.state.hideCompleted
-    });
-  },
+	toggleHideCompleted() {
+		this.setState({
+			hideCompleted: ! this.state.hideCompleted
+		});
+	},
 
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <h1>Todo List ({this.data.incompleteCount})</h1>
-          <label className="hide-completed">
-            <input
-              type="checkbox"
-              readOnly={true}
-              checked={this.state.hideCompleted}
-              onClick={this.toggleHideCompleted} />
-            Hide Completed Tasks
-          </label>
-          <form className="new-task" onSubmit={this.handleSubmit} >
-            <input
-              type="text"
-              ref="textInput"
-              placeholder="Type to add new tasks" />
-          </form>
-        </header>
-        <ul>
-          {this.renderTasks()}
-        </ul>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="container">
+				<header>
+					<h1>Todo List ({this.data.incompleteCount})</h1>
+					<label className="hide-completed">
+						<input
+							type="checkbox"
+							readOnly={true}
+							checked={this.state.hideCompleted}
+							onClick={this.toggleHideCompleted} />
+						Hide Completed Tasks
+					</label>
+
+					<AcccountsUIWrapper />
+
+					{ 
+						this.data.currentUser ?
+						<form className="new-task" onSubmit={this.handleSubmit} >
+							<input
+								type="text"
+								ref="textInput"
+								placeholder="Type to add new tasks" />
+						</form> : ''
+					}
+					
+				</header>
+				<ul>
+					{this.renderTasks()}
+				</ul>
+			</div>
+		);
+	}
 });
--- Task.jsx
+++ Task.jsx
@@ -33,7 +33,9 @@ Task = React.createClass({
 					checked={this.props.task.checked}
 					onClick={this.toggleChecked} />
 
-				<span className="text">{this.props.task.text}</span>
+				<span className="text">
+					<b>{this.props.task.username ? this.props.task.username : 'unknown'}: </b>{this.props.task.text}
+				</span>
 			</li>
 		);
 	}
