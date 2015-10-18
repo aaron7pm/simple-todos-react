ac9c4ffad56be4314d5879b9a1695269d6adba58
Aaron Dancer <me@aarondancer.com>
2015-10-18 00:02:43 -0500

    Added user validation and private tasks

--- Task.jsx
+++ Task.jsx
@@ -3,7 +3,8 @@ Task = React.createClass({
 	propTypes: {
 		// This component gets the task to display through a React prop.
 		// We can use propTypes to indicate it is required
-		task: React.PropTypes.object.isRequired
+		task: React.PropTypes.object.isRequired,
+		showPrivateButton: React.PropTypes.bool.isRequired
 	},
 
 	toggleChecked() {
@@ -14,10 +15,15 @@ Task = React.createClass({
 		Meteor.call("removeTask", this.props.task._id);
 	},
 
+	togglePrivate() {
+		Meteor.call("setPrivate", this.props.task._id, ! this.props.task.private);
+	},
+
 	render() {
 		// Give tasks a different className when they are checked off,
 		// so that we can style them nicely in CSS
-		const taskClassName = this.props.task.checked ? "checked" : "";
+		const taskClassName = (this.props.task.checked ? "checked" : "") + " " +
+			(this.props.task.private ? "private" : "");
 
 		return (
 			<li className={taskClassName}>
@@ -30,6 +36,12 @@ Task = React.createClass({
 					readOnly={true}
 					checked={this.props.task.checked}
 					onClick={this.toggleChecked} />
+
+				{ this.props.showPrivateButton ? (
+					<button className="toggle-private" onClick={this.togglePrivate}>
+						{ this.props.task.private ? "Private" : "Public" }
+					</button>
+				) : '' }
 
 				<span className="text">
 					<b>{this.props.task.username ? this.props.task.username : 'unknown'}: </b>{this.props.task.text}
--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -3,37 +3,60 @@ Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
   // This code is executed on the client only
-  
   Accounts.ui.config({
-		passwordSignupFields: "USERNAME_ONLY"
+    passwordSignupFields: "USERNAME_ONLY"
   });
+
+  Meteor.subscribe("tasks");
 
   Meteor.startup(function () {
-		// Use Meteor.startup to render the component after the page is ready
-		React.render(<App />, document.getElementById("render-target"));
-  });
-
-  Meteor.methods({
-		addTask(text) {
-			// Make sure the user is logged in before inserting a task
-			if (! Meteor.userId()) {
-				throw new Meteor.Error("not-authorized");
-			}
-
-			Task.insert({
-				text: text,
-				createdAt: new Date(),
-				owner: meteor.userId(),
-				username: Meteor.user().username
-			});
-		},
-
-		removeTask(taskId) {
-			Tasks.remove(taskId);
-		},
-
-		setChecked(taskId, setChecked) {
-			Tasks.update(taskId, { $set: { checked: setChecked } });
-		}
+    // Use Meteor.startup to render the component after the page is ready
+    React.render(<App />, document.getElementById("render-target"));
   });
 }
+
+if (Meteor.isServer) {
+	// Only publish tasks that are public or belong to the current user
+  Meteor.publish("tasks", function () {
+    return Tasks.find({
+    	$or: [
+    		{ private : {$ne: true} },
+    		{ owner: this.userId }
+    	]
+    });
+  });
+}
+
+Meteor.methods({
+  addTask(text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+
+  removeTask(taskId) {
+    Tasks.remove(taskId);
+  },
+
+  setChecked(taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  },
+
+  setPrivate(taskId, setToPrivate) {
+  	const task = Tasks.findOne(taskId);
+
+  	// Make sure only the task owner can make a task private
+  	if (task.owner !== Meteor.userId()) 
+  		throw new Meteor.Error("not-authorized");
+
+  	Tasks.update(taskId, { $set: { private: setToPrivate } });
+  }
+});
